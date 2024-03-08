75f8041f5f60395d71c0dde461e90f0c55f15064
DrSaymon <impair36@gmail.com>
2024-03-09 00:47:45 +0700

    complite game

--- game.py
+++ game.py
@@ -1,9 +1,12 @@
 'Game: guess number'
 import numpy as np
-number = np.random.randit(1, 101)
+number = np.random.randint(1, 101)
 
 count = 0
 
 while True:
     count+=1
-    predict_number = int(input('Guess a number between 1 and 100'))
+    predict_number = int(input('Guess a number between 1 and 100: '))
+    if predict_number == number:
+        print(f'Correct guess! It was took a {count} tries!')
+        break
